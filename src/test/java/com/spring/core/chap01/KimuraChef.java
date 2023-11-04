d45d36ec244ea576cc8dd4f5d2c6fe0e6f38d9e1
SeungHanLee <swq3064@naver.com>
2023-11-04 10:37:17 +0900

    스프링 안쓴 원리 이해 위한 코드

---
+++ src/test/java/com/spring/core/chap01/KimuraChef.java
@@ -0,0 +1,8 @@
+package com.spring.core.chap01;
+
+public class KimuraChef implements Chef {
+
+    public void cook() {
+        System.out.println("스시 명장 키무라 입니다.");
+    }
+}
---
+++ src/test/java/com/spring/core/lombok/Person.java
@@ -0,0 +1,10 @@
+package com.spring.core.lombok;
+
+import lombok.Setter;
+
+@Setter
+public class Person {
+
+    private String name;
+    private int age;
+}
